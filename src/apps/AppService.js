ef2e914aebec50926b6315a2f7adc12ff240862c
Byung Hoon Cho <byunghoon.cho@aquaapps.co>
2016-07-13 14:06:54 +1200

    Fix app info width on large screens

--- src/apps/AppService.js
+++ src/apps/AppService.js
@@ -17,16 +17,16 @@
                 amazon: 'http://www.amazon.com/AQUA-Apps-Learn-Basic-Latin/dp/B00E7QVRZA/'
             },
             {
-                name: 'Triplanner',
-                icon: 'ic-triplanner',
-                color: "#1377BA",
-                colorLight: "#309FEA",
-                colorDark: "#0c4973",
-                description: 'Planning a holiday? Use Triplanner to plan everything. It\'s simple and fast!',
-                fullDescription: 'Planning a holiday? Use Triplanner to plan everything. It\'s simple and fast! Set the start time of the event, how long it will last, and the locations. See each day\'s overview at a glance, and just tap an event to see more detail.<br><br>Using Google\'s Material design, combined with an intuitive interface, this trip planner makes organizing your holiday fast, simple and painless.',
-                legal: '',
-                google: 'https://play.google.com/store/apps/details?id=com.aqualabs.triplanner',
-                amazon: 'http://www.amazon.com/AQUA-Apps-Triplanner/dp/B00QSWCMLE/'
+                name: 'Convert',
+                icon: 'ic-convert',
+                color: "#469E4A",
+                colorLight: "#71C074",
+                colorDark: "#285A2A",
+                description: 'Need a simple to use and lightweight unit converter? Download Convert now!',
+                fullDescription: 'Need a simple to use and lightweight unit converter? Download Convert now! This Material Design app has over 20 converters with more to come soon<ul><li>Over 20 converters in 6 categories (basic, digital, electricity, engineering, waves, other)</li><li>Search through converters</li><li>Auto detect (beta<sup class="legal-sup">1</sup>): don\'t know exactly what unit you want to convert? Type it into auto detect, and let the app figure it out!</li><li>Text replace<sup class="legal-sup">2</sup>: when you\'re browsing the web, or having a conversation, simply highlight the text you want to convert, and press "Convert." If it\'s in an editable text field, you can even replace the text with the converted units. Never leave an app to convert a unit again</li><li>Material design</li><li>Tablet UI</li></ul>',
+                legal: '<sup class="legal-sup">1</sup> This feature is currently being tested, and the reliability of detecting units cannot be guaranteed<br><sup class="legal-sup">2</sup> Available on Android 6.0 (Marshmallow) or newer',
+                google: 'https://play.google.com/store/apps/details?id=co.aquaapps.convert',
+                amazon: 'http://www.amazon.com/AQUA-Apps-Convert/dp/B01CMHECXY/'
             },
             {
                 name: 'Squash',
@@ -41,23 +41,18 @@
                 amazon: ''
             },
             {
-                name: 'Convert',
-                icon: 'ic-convert',
-                color: "#469E4A",
-                colorLight: "#71C074",
-                colorDark: "#285A2A",
-                description: 'Need a simple to use and lightweight unit converter? Download Convert now!',
-                fullDescription: 'Need a simple to use and lightweight unit converter? Download Convert now! This Material Design app has over 20 converters with more to come soon<ul><li>Over 20 converters in 6 categories (basic, digital, electricity, engineering, waves, other)</li><li>Search through converters</li><li>Auto detect (beta<sup class="legal-sup">1</sup>): don\'t know exactly what unit you want to convert? Type it into auto detect, and let the app figure it out!</li><li>Text replace<sup class="legal-sup">2</sup>: when you\'re browsing the web, or having a conversation, simply highlight the text you want to convert, and press "Convert." If it\'s in an editable text field, you can even replace the text with the converted units. Never leave an app to convert a unit again</li><li>Material design</li><li>Tablet UI</li></ul>',
-                legal: '<sup class="legal-sup">1</sup> This feature is currently being tested, and the reliability of detecting units cannot be guaranteed<br><sup class="legal-sup">2</sup> Available on Android 6.0 (Marshmallow) or newer',
-                google: 'https://play.google.com/store/apps/details?id=co.aquaapps.convert',
-                amazon: 'http://www.amazon.com/AQUA-Apps-Convert/dp/B01CMHECXY/'
+                name: 'Triplanner',
+                icon: 'ic-triplanner',
+                color: "#1377BA",
+                colorLight: "#309FEA",
+                colorDark: "#0c4973",
+                description: 'Planning a holiday? Use Triplanner to plan everything. It\'s simple and fast!',
+                fullDescription: 'Planning a holiday? Use Triplanner to plan everything. It\'s simple and fast! Set the start time of the event, how long it will last, and the locations. See each day\'s overview at a glance, and just tap an event to see more detail.<br><br>Using Google\'s Material design, combined with an intuitive interface, this trip planner makes organizing your holiday fast, simple and painless.',
+                legal: '',
+                google: 'https://play.google.com/store/apps/details?id=com.aqualabs.triplanner',
+                amazon: 'http://www.amazon.com/AQUA-Apps-Triplanner/dp/B00QSWCMLE/'
             }
         ];
-
-        return {
-            loadAllApps : function() {
-                return $q.when(apps);
-            }
-        };
+        return { loadAllApps : function() { return $q.when(apps); } };
     }
 })();
